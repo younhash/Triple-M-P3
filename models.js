a99f4b7453bbba7367cb753d5ffa0286c92f85a0
Marcos Dominguez <primepowermech@gmail.com>
2019-10-21 15:23:34 -0400

    trails, cities and users models have been created

---
+++ models.js
@@ -0,0 +1,60 @@
+import { Model, DataTypes, Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize(
+    {
+        database: "bikingtrails_db",
+        dialet: "postgres"
+    }
+)
+export class Trails extends Model{}
+
+Trail.init(
+    {
+      name: DataTypes.STRING,
+      summary: DataTypes.STRING,
+      image: DataTypes.STRING,
+      city: DataTypes.STRING,
+      state: DataTypes.STRING,
+      difficulty: DataTypes.STRING,
+      length: DataTypes.INTEGER,
+      latitude: DataTypes.INTEGER,
+      longitude: DataTypes.INTEGER,
+      url: DataTypes.STRING
+    },
+    {
+    freezeTableName: true,
+    tableName: "Trails",
+    sequelize
+    },
+)
+
+export class City extends Model{}
+
+    City.init(
+        {
+            name: DataTypes.STRING,
+            latitude: DataTypes.INTEGER,
+            longitude: DataTypes.INTEGER,
+            state: DataTypes.STRING
+        },
+        {
+            freezeTableName: true,
+            tableName: "Cities",
+            sequelize
+        }
+    )
+
+    export class User extends Model{}
+
+    User.init(
+        {
+            firstName: DataTypes.STRING,
+            lastName: DataTypes.STRING,
+            alias: DataTypes.STRING,
+        },
+        {
+            freezeTableName: true,
+            tableName: "Users",
+            sequelize
+        }
+    )
