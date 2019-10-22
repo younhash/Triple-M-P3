a0396e37ee81e87343029409d46ce51c033675a1
Marcos Dominguez <primepowermech@gmail.com>
2019-10-22 09:56:53 -0400

    deleted mp3 database

--- models.js
+++ models.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { Model, DataTypes, Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize(
-    {   database: "mp3",
-        dialect: "postgres",
-    }
-)
-=======
 import { Model, DataTypes, Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize(
