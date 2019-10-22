1080dc5ac50c7c4d6f2d884c79cafb25e0101a03
Marcos Dominguez <primepowermech@gmail.com>
2019-10-22 15:06:02 -0400

    added an update trail, find a trail by id and get all users

--- routes.js
+++ routes.js
@@ -1,23 +1,8 @@
 import express from 'express'
 import { Router } from 'express'
 import {Trail, City, User} from './models'
-const getAllTrails = async (
-    /** @type{express.Request}*/ req,
-    /** @type{express.Response}*/ res
-    ) => {
-        try{
-            const trails = await Trail.findAll()
-            console.log("trails", Trail);
-            
-            return res.status(200).json({trails}) 
-        }catch (error) {
-            return res.status(500).send(error.message)
-        }
-    }
-export const allTrailsRouter = Router()
-allTrailsRouter.get("/trails", getAllTrails)
 
-export const allCitiesRouter = Router() 
+ 
 const getAllCities = async (
     /**@type{express.Request}*/req,
     /**@type{express.Response}*/res) => {
@@ -28,8 +13,10 @@ const getAllCities = async (
         return res.status(500).send(error.message)
     }
 }
+export const allCitiesRouter = Router()
 allCitiesRouter.get("/cities",getAllCities)
-export const cityRouter = Router()
+
+
 const getCityByID = async (
     /**@type{express.Request}*/req,
     /**@type{express.Response}*/res) => {
@@ -43,15 +30,16 @@ const getCityByID = async (
         return res.status(500).send(error.message)
     }
 }
-cityRouter.get("/cities/:id", getCityByID)
+allCitiesRouter.get("/cities/:id", getCityByID)
 
 
-export const userRouter = Router()
 const addNewUser = (/** @type {express.Request} */req, /** @type {express.Response} */ res) => {
     let {firstName, lastName, alias} = req.body
     return User.create({firstName, lastName, alias}).then(user => {return res.status(200).json({created: user}) })
     .catch(error => {return res.status(500).send(error.message)})
 }
+
+
 const getAllUsers = async (
     /**@type{express.Request}*/req,
     /**@type{express.Response}*/res) => {
@@ -62,7 +50,45 @@ const getAllUsers = async (
         return res.status(500).send(error.message)
     }
 }
-
-
+export const userRouter = Router()
 userRouter.get('/users', getAllUsers)
 userRouter.post("/users", addNewUser)
+
+const getAllTrails = async (
+    /** @type{express.Request}*/ req,
+    /** @type{express.Response}*/ res
+    ) => {
+        try{
+            const trails = await Trail.findAll()
+            console.log("trails", Trail);
+            
+            return res.status(200).json({trails}) 
+        }catch (error) {
+            return res.status(500).send(error.message)
+        }
+    }
+const getTrailByID = async (
+    /**@type{express.Request}*/req,
+    /**@type{express.Response}*/res) => {
+        
+    try{
+        const id = req.params.id
+        const trails = await Trail.findOne({
+            where: {id: id}
+        })
+        return res.status(200).json({trails}) 
+    }catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+const updateTrail = (/** @type {express.Request} */ req, /** @type {express.Response} */ res)=>{
+    return Trail.update(req.body, {where: {id: req.params.id}}).then(trail => {
+        return res.status(200).json({updated: trail})
+    }).catch(error => { return res.status(500).send(error.message)})
+}
+export const allTrailsRouter = Router()
+allTrailsRouter.get("/trails", getAllTrails)
+allTrailsRouter.get("/trails/:id", getTrailByID)
+allTrailsRouter.put("/trails/:id/edit", updateTrail)
+
+
