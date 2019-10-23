f623691305380b7e16e1cb3cb0e60435f73e34b3
Mohammad Jaiteh <jaitehmohammad.com>
2019-10-23 15:31:42 -0400

    nav and search much better

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,12 +7,12 @@ export default function Nav({searchChange, search}){
     
     return(
         <div className='nav'>
-            <img src='/crop-david-marcu-unsplash.jpg' alt='bike riding in the mountain' width="100%" height=""/>
             <div className="links">
                 <Link to='/'>Home</Link>
                 <Link to='/states'>States</Link>
                 <Link to='/trails'>Trails</Link>
             </div> 
+            <img src='/crop-david-marcu-unsplash.jpg' alt='bike riding in the mountain' width="100%" height=""/>
             <Search fxns={{searchChange}} search={search} />
             {/* <ModelList model={'state'} search={search} /> */}
         </div>
