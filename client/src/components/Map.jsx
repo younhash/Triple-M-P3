8c9aefb5cb0334eeac49a2fcd26e42c2b2faf3eb
Marcos Dominguez <primepowermech@gmail.com>
2019-10-23 14:32:57 -0400

    added mapping code and deleted test.js

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
-export default function Map({info, model}){
-    // console.log('model',model);
-    // console.log('ze info',info);
+import ReactMapGL from 'react-map-gl'
+export default function Map(){
+    const viewPortWidth = () => {
+        if(window.innerWidth < 700){
+        return window.innerWidth
+        }return window.innerWidth*.6
     
-    // let {lat, lon} = cityInfo
-    return(
-        <div className='map'>
+    }
+    const viewPortHeight = () => {
+        if(window.innerWidth < 700){
+        return window.innerHeight*.5
+        }return window.innerHeight
+    
+    }
+    const viewPortZoom = () => {
+      if(window.innerWidth < 700){
+      return 10
+      }return 11.75
+    
+    }
+    
+    const [viewport, setViewport] = useState({
+      width:viewPortWidth(),
+      height:viewPortHeight(),
+      latitude: 40.7433,
+      longitude: -73.9485,
+      zoom:viewPortZoom()
+    });
+    
+    
+    const changeViewPort = () =>{
+    
+      const newport = {
+        ...viewport,
+        width:viewPortWidth(),
+        height:viewPortHeight(),
+        
+      }
+      setViewport(newport)
+    }
+    
+    useEffect(() => {
+      window.addEventListener("resize", changeViewPort)
+    }, []);
+    
+    const _onViewportChange = viewport => {
+      setViewport(viewport)
+    }
+    
+      return (
+        <div className="App">
+         <ReactMapGL
+                    width={viewport.width}
+                    height={viewport.height}
+                    latitude={viewport.latitude}
+                    longitude={viewport.longitude}
+                    zoom={viewport.zoom}
+                    mapboxApiAccessToken='pk.eyJ1IjoiYmFydGNvbnNlZGluZSIsImEiOiJjazBudWVxajUwMXdlM2hwZzFzcDQ5cWR5In0.376OjUpSFMy-y-PVfAeO9A'
+                    
+                    onViewportChange={_onViewportChange}
+                >
+          </ReactMapGL>
         </div>
-    )
-}
+      );
+    }
+    
--- client/src/components/user/EditUser.jsx
+++ client/src/components/user/EditUser.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
-export default function EditUser(){
+import React, {useState} from 'react';
+import axios from 'axios';
+
+export default function EditUser({userObj, setUserObj}){
+    let [updatedUserDesc, setUpdatedUserDesc] = useState(userObj['summary']);
+
+    const editDescription = (e) => {setUpdatedUserDesc(e.target.value)}
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        // make the call
+        let query = `/api/users/${userObj.id}/edit`
+        let newUserObj = {...userObj, 'summary': updatedUserDesc}
+        try {
+            await axios.put(query, newUserObj);
+            setUserObj(newUserObj)
+        } catch (err) {console.log(err)}
+    }
+    const blurHandler = async (e) => {setUserObj({...userObj})}
+    let firstNameElem = <input className='input-update-user-description' type='text' onChange={editDescription} value={updatedUserDesc} onBlur={blurHandler} autoFocus />
+    let lastNameElem = <input className='input-update-user-description' type='text' onChange={editDescription} value={updatedUserDesc} onBlur={blurHandler} autoFocus />
+    let aliasElem = <input className='input-update-user-description' type='text' onChange={editDescription} value={updatedUserDesc} onBlur={blurHandler} autoFocus />
+
     return(
         <div className='user-edit'>
+            <div className='user-edit-card' >
+                <form onSubmit={submitHandler} >
+                    {firstNameElem}
+                    {lastNameElem}
+                    {aliasElem}
+                </form>
+            </div>
         </div>
     )
 }
