faa42bca66f8d0c6fc894113789e2b3e786134a2
Marcos Dominguez <primepowermech@gmail.com>
2019-10-23 15:29:59 -0400

    added useEffect to map, and adjusted edit user

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import ReactMapGL from 'react-map-gl'
 export default function Map(){
     const viewPortWidth = () => {
