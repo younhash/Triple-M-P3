4f73643d9ed5761c446904595d4e01173e35949c
mattrlucciola <mr3lucciola@gmail.com>
2019-10-24 11:49:06 -0400

    issue with createtrail

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,6 @@ function App() {
   }
 
   return (
-    
     <BrowserRouter>
       <Doc />
       <div className="App">
--- client/src/components/trail/CreateTrail.jsx
+++ client/src/components/trail/CreateTrail.jsx
@@ -11,16 +11,21 @@ export default function CreateTrail(){
         state: '',
         difficulty: '',
         length: 0,
-        latitude: null,
-        longitude: null
+        latitude: '',
+        longitude: ''
     })
+
+    console.log(trailObj);
+    
     // console.log('keys', {...Object().keys()});
     let [formArr, setFormArr] = useState([]);
     const submitHandler = async (e) => {
         e.preventDefault();
+
         // make the call
         let query = `/api/trails/`
         // let userObj = {firstName, lastName, alias};
+
         try {
             let res = await axios.post(query, trailObj);
             console.log(res);
@@ -29,27 +34,22 @@ export default function CreateTrail(){
             document.location = '/trails'
         } catch (err) {console.log(err)}
     }
-
-    /*
-    name: '',
-    summary: '',
-    imgSmall: 'url',
-    imgMedium: 'url',
-    city: '',
-    state: '',
-    difficulty: '',
-    length: 0,
-    latitude: null,
-    longitude: null,
-    */
+    const changeHandler = (e, key) => {
+        setTrailObj({...trailObj, [key]:e.target.value});
+        console.log({...trailObj, [key]:e.target.value});
+    }
     const buildForm = () => {
         let formArr = [];
         for (let key in trailObj) {
-            let formElem = (
+            let formElem = 
                 <div className={`new-trail-${key}`} >
-                    <input onChange={()=>{}} value={trailObj[key]}/>
+                    {key}<input onChange={
+                        (e) => {
+                            setTrailObj({...trailObj, [key]:e.target.value});
+                            console.log(e.target.value);
+                        }
+                    } value={trailObj[key]}/>
                 </div>
-            )
             formArr.push(formElem);
         }
         formArr.push(
@@ -57,12 +57,7 @@ export default function CreateTrail(){
                 <button type='submit'>Submit</button>
             </div>
         );
-
         let trailFormArr = formArr
-            // <form onSubmit={submitHandler}>
-                
-            // {/* </form> */}
-        
         setFormArr([...trailFormArr])
     }
     useEffect(()=>{
@@ -71,21 +66,6 @@ export default function CreateTrail(){
     return(
         <div className='create-trail'>
             <form onSubmit={submitHandler} >
-                {/* <div className="first-name">
-                    {`First Name: `}
-                    <input type='text' onChange={(e) => {setFirstName(e.target.value)}} value={firstName} />
-                </div>
-                <div className="last-name">
-                    {`Last Name: `}
-                    <input type='text' onChange={(e) => {setLastName(e.target.value)}} value={lastName} />
-                </div>
-                <div className="alias">
-                    {`Username: `}
-                    <input type='text' onChange={(e) => {setAlias(e.target.value)}} value={alias} />
-                </div> */}
-                {/* <div className="submit-trail">
-                    <button type='submit'>Submit</button>
-                </div> */}
                 {formArr}
             </form>
         </div>
